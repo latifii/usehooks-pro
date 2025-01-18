bfc7015c618f5ad9d59bae177e550b42f4f02c5d
latifii <latifii68@gmail.com>
2025-01-18 17:55:14 +0330

    add jest.js

---
+++ jest.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+  },
+  testMatch: ["**/?(*.)+(spec|test).[tj]s?(x)"],
+};
